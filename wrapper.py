8ee779d77861705dd28f1aa16cb6a4fbf1995a01
JellyLii <j.aligno97@gmail.com>
2022-11-14 01:16:31 +1100

    Update wrapper.py
    - Added object attributes
    - Added logic to skip redirects to main page, Added logic to handle multiple material possibilities
    -Fixed regex patterns to work correctly
    -Other things that I don't remember doing as well.

--- wrapper.py
+++ wrapper.py
@@ -5,7 +5,17 @@ import requests
 import sys
 
 class gear:
-    def __init__(self,url,product_code,brand,price_acutal,price_regular):
+
+    __attrs__ = [
+        "url",
+        "product_code",
+        "brand",
+        "price_actual",
+        "price_regular",
+        "material",
+    ]
+
+    def __init__(self,url,product_code,brand,price_actual,price_regular,material):
         '''
         sizes
         size_chart
@@ -14,24 +24,51 @@ class gear:
         self.url = url
         self.product_code = product_code
         self.brand = brand
-        self.price_actual = price_acutal
+        self.price_actual = price_actual
         self.price_regular = price_regular
-
+        self.material = material
 
 class muaythaifactory:
-    def __init__(self,gear_type):
-        self.domain = 'https://www.muaythaifactory.com/'
 
+    __attrs__ = [
+        "domain",
+        "err_page"
+        "session",
+        "gear_type",
+        "path",
+        "working_url",
+    ]
+
+    def __init__(self,gear_type=""):
+        self.domain = 'https://www.muaythaifactory.com/'
+        self.err_page = 'https://www.muaythaifactory.com/?E=SNOTFOUND'
+
+        self.session = requests.Session()
+
+        self.gear_type = gear_type
+        self.path = ""
+        self.working_url = ""
+
+        if not(self.gear_type == ""):
+            self.setGearType(self.gear_type)
+
+    def closeSession(self):
+        self.session.close()
+
+    def setGearType(self,gear_type):
+        #TODO: add all categories
         try:
             if (gear_type == 'shorts'):
+                self.gear_type = 'shorts'
                 self.path = 'muay-thai-shorts.asp'
             else:
                 raise ValueError
-        except:
-            print("Wrong input, Failure awww")
-            sys.exit(1)
 
-        self.working_url = self.domain + self.path
+            self.working_url = self.domain + self.path
+
+        except:
+            print("Wrong input, Failure aww")
+            sys.exit(1)
 
     def getGearListPages(self,page0):
         pattern = re.compile(r'(?<=page \d+ of )\d+')
@@ -39,12 +76,15 @@ class muaythaifactory:
 
     def getPage(self,url):
         try:
-            response = requests.get(url)
+            response = self.session.get(url)
 
             if (response.status_code == 200):
-                return response.text
+                if (response.url == self.err_page):
+                    return None
+                else:
+                    return response.text
             else:
-                raise Error
+                raise IndexError
         except:
             print("Couldn't get to webpage:" + url)
             sys.exit(1)
@@ -82,23 +122,33 @@ class muaythaifactory:
         return set(prod_urls)
 
     def getGearInfo(self,url):
+        if (gear_page_html := self.getPage(url)) is None:
+            return None
+       
         prod_code = self.getProdCode(url)
-
-        gear_page_html = self.getPage(url)
-
         brand = self.getBrand(gear_page_html)
         price_actual = self.getActualPrice(gear_page_html)
         price_regular = self.getRegularPrice(gear_page_html)
         material = self.getMaterial(gear_page_html)
 
-        return gear(url,prod_code,brand,price_actual,price_regular)
+        return gear(url,prod_code,brand,price_actual,price_regular,material)
+
+    def getAllGear(self,limit=-1):
+        gear_list = []
+        prod_urls = self.findAllProductURLs(limit)
+
+        for url in prod_urls:
+            if (curr_gear := self.getGearInfo(url)) is not None:
+                gear_list.append(curr_gear)
+
+        return gear_list
 
     def getProdCode(self,url):
         pattern = re.compile(r'(?<=ProductID=).+')
         return pattern.search(url).group()
 
     def getBrand(self,html):
-        pattern = re.compile(r'(?<=<meta itemprop="name" content=")\w+(?=">)')
+        pattern = re.compile(r'(?<=<meta itemprop="name" content=")[\w\s]+(?=">)')
         return pattern.search(html).group()
 
     def getActualPrice(self,html):
@@ -110,6 +160,5 @@ class muaythaifactory:
         return float(pattern.search(html).group())
 
     def getMaterial(self,html):
-        pattern = re.compile(r'(?<=<div class="product-info-subtext "  >Material: <span id="material-single" data-code="1">)\w+(?=</span>)')
-        return pattern.search(html).group()
-
+        pattern = re.compile(r'(?<=<span .+material.+>)[\w\s]{2,}(?=<\/span>)')
+        return pattern.findall(html)
