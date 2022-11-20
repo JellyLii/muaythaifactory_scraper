a50d63e4acca2f5314689311557b60584c3a62a8
JellyLii <j.aligno97@gmail.com>
2022-11-20 23:10:35 +1100

    Changed during testing
    Not actually sure whether anything changed

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from scraper import *
 
 def main():
-    try:
-        x = muaythaifactory('')
-        x.setGearType('all-gloves')
+    x = muaythaifactory('')
+    x.setGearType('all-gloves')
 
-        gear_list = x.getAllGear()
+    gear_list = x.getAllGear()
 
-        for gear in gear_list:
-            print(gear)
-        
-        x.closeSession()
-    except:
-        print("ran into problem")
-        x.closeSession()
-        sys.exit(0)
+    for gear in gear_list:
+        print(gear)
+    
+    x.closeSession()
 
 if __name__ == "__main__":
     main()
