156ab2c16025fe35d20311eb5f3acf3dbbd84890
JellyLii <j.aligno97@gmail.com>
2022-11-26 23:19:07 +1100

    Modified to work with CSV implementation

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from scraper import *
 
 def main():
-    gear_type = 'all-gloves'
+    gear_type = 'mma-gloves'
     with muaythaifactory(gear_type) as x:
 
-        gear_list = x.getAllGear()
+        gear_list = x.csvAllGear()
 
-        for gear in gear_list:
-            print(gear)
-
-        
 if __name__ == "__main__":
     main()
