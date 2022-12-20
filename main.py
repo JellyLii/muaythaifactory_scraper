1424c1dab4c71553ed62f929d873693975c1a127
JellyLii <j.aligno97@gmail.com>
2022-12-20 22:56:28 +1100

    fixed conflicts

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from scraper import *
 
 def main():
-<<<<<<< HEAD
     gear_type = 'all-gloves'
-=======
-    gear_type = 'mma-gloves'
->>>>>>> a5d3dbe4602c9f5ed9973c5626312be8a79f5653
 
     with muaythaifactory(gear_type) as x:
         print(x.csv_filepath)
